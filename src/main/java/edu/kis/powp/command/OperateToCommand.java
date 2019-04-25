746211b12abe6bf5fbc46b20f9cc81ff713e9a8c
Sosnus <sosnus15@gmail.com>
2019-04-25 15:46:09 +0200

    backup from lab 25.04.2019

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -1,9 +1,12 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.drivers.DriverManager;
+
 public class OperateToCommand  implements DriverCommand {
     public void execute()
     {
-
+        DriverManager test = new DriverManager();
+        test.setCurrentDriver(this.x,this.y); //replace xy to Driver
     }
 
     private int x;
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -1,9 +1,12 @@
 package edu.kis.powp.command;
 
+//import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.adapter.MyAdapter;
+
 public class SetPositionCommand implements DriverCommand {
     public void execute()
     {
-
+        MyAdapter.setPosition();
     }
 
     private int x;
